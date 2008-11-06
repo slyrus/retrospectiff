4db97cd3eff74810f08480da6910bf4f7afc5f3d
Cyrus Harmon <ch-lisp@bobobeach.com>
2008-11-05 22:12:49 -0800

    update README. add copyright notices to source files. delete unused (and empty) make-ifd-entry function.

--- retrospectiff.lisp
+++ retrospectiff.lisp
@@ -1,3 +1,32 @@
+;;; file: retrospectiff.lisp
+;;; author: cyrus harmon
+;;;
+;;; Copyright (c) 2008 Cyrus Harmon (ch-lisp@bobobeach.com)
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+;;;
+;;;   * Redistributions of source code must retain the above copyright
+;;;     notice, this list of conditions and the following disclaimer.
+;;;
+;;;   * Redistributions in binary form must reproduce the above
+;;;     copyright notice, this list of conditions and the following
+;;;     disclaimer in the documentation and/or other materials
+;;;     provided with the distribution.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (in-package :retrospectiff)
 
@@ -460,9 +489,6 @@
 (defun read-tiff-file (pathname)
   (with-open-file (stream pathname :direction :input :element-type :default)
     (read-tiff-stream stream)))
-
-(defun make-ifd-entry (tag field-type value-count values)
-  )
 
 ;; we should return the number of strips (and possibly the length of
 ;; each strip (uncompressed), but not yet)..
