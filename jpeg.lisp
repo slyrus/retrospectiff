3e1a08b16ad55cf7decfe1814e608fe458d2457a
Cyrus Harmon <ch-github@bobobeach.com>
2017-03-30 12:53:55 -0700

    properly use jpeg descriptor to avoid using unexported interfaces
     * we make a concatenated-stream (appropriately hacked up, of course)
       and a custom byte-reader for it and use these in the call to
       jpeg:make-descriptor such that we can just call decode-stream,
       rather than having our own cut-and-pasted, half-baked
       implementation of decode-stream here.
    
     * note that this requires the change on the
       descriptor-byte-reader-fix cl-jpeg branch. hopefully this will hit
       master soon.

--- jpeg.lisp
+++ jpeg.lisp
@@ -6,36 +6,30 @@
     (declare (ignore stream))
     (error "JPEG encoding not supported yet!")))
 
-(defvar *jpeg-stream*)
-
-(defun jpeg-stream-reader ()
-  (read-byte *jpeg-stream*))
-
-(defun read-jpeg-tables (jpeg-image-info)
-  (let* ((*jpeg-stream* (flexi-streams:make-in-memory-input-stream
-                         (jpeg-tables jpeg-image-info))))
-    (let ((image (jpeg::make-descriptor
-                  :byte-reader #'jpeg-stream-reader)))
-      (unless (= (jpeg::read-marker image) jpeg::+M_SOI+)
-        (error "Unrecognized JPEG format"))
-      (let ((marker (jpeg::interpret-markers image 0)))
-        (unless (= marker jpeg::+M_EOI+)
-          (error "Unrecognized JPEG format")))
-      (setf (jpeg-image jpeg-image-info) image))))
-
 (defun jpeg-decode (compressed-vector jpeg-image-info)
   ;;  NOTE: we currently read the jpeg-tables every time through. We
   ;;  should be able to cache this, but we don't as its state gets
   ;;  modified by the JPEG reading stuff and we can't just reuse it
   ;;  each time through. So..., for the moment at least, we make a new
   ;;  one each time through.
-  (read-jpeg-tables jpeg-image-info)
-  (let ((image (jpeg-image jpeg-image-info)))
-    (let ((*jpeg-stream* (flexi-streams:make-in-memory-input-stream compressed-vector)))
-      (unless (= (jpeg::read-marker image) jpeg::+M_SOI+)
-        (error "Unrecognized JPEG format"))
-      (let* ((marker (jpeg::interpret-markers image 0)))
-        (cond ((= jpeg::+M_SOF0+ marker)
-               (jpeg::decode-frame image nil)
-               (jpeg::descriptor-buffer image))
-              (t (error "Unsupported JPEG format: ~A" marker)))))))
+  (let* ((tables (jpeg-tables jpeg-image-info))
+         (tables-stream
+          ;; We need to trim off the last two bytes of the tables
+          ;; vector as the jpeg-stream-reader code isn't expecting
+          ;; the EOI marker.
+          (flexi-streams:make-in-memory-input-stream (subseq tables 0 (- (length tables) 2))))
+         (jpeg-data-stream
+          ;; we also need to trim off the first two bytes from this
+          ;; stream, but it's probably better to make the stream and
+          ;; call read-byte twice than to copy the data vector.
+          (flexi-streams:make-in-memory-input-stream compressed-vector)))
+    ;; See comment above. Now skip the two bytes in the data stream.
+    (read-byte jpeg-data-stream)
+    (read-byte jpeg-data-stream)
+    (let ((jpeg-stream
+           (make-concatenated-stream tables-stream jpeg-data-stream)))
+      (flet ((jpeg-stream-reader ()
+               (read-byte jpeg-stream)))
+        (let ((image-desc (jpeg:make-descriptor :byte-reader #'jpeg-stream-reader)))
+          (jpeg:decode-stream nil :descriptor image-desc :cached-source-p t))))))
+
